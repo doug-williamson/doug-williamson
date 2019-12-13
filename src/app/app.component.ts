51c0ab36e532ff11284dc827cce5143dbe2e5cf6
Doug Williamson <douglasmwilliamson@gmail.com>
2019-12-13 09:32:14 -0500

    1.0.0-beta.0 updates
    Updated primary views for Beta.0 release

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RhombusShellNavItem } from '@dougwilliamson/rhombus-shell';
+import { RhombusShellThemeService } from '@dougwilliamson/rhombus-shell';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'doug-williamson';
+export class AppComponent implements OnInit{
+
+  _isDarkTheme: Observable<boolean>;
+  title = 'Doug Williamson';
+
+  navItems: RhombusShellNavItem[] = [
+    {
+      label: 'Biography',
+      route: 'biography',
+    },
+    {
+      label: 'Experience',
+      route: 'experience',
+    },
+    {
+      label: 'Rhombus TV',
+      route: 'streaming',
+    },
+  ];
+
+  constructor(
+    private themeService: RhombusShellThemeService) { }
+
+  ngOnInit() {
+    this._isDarkTheme = this.themeService.darkThemeSubject.asObservable();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { RhombusShellModule } from '@dougwilliamson/rhombus-shell';
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule, MatListModule, MatIconModule, MatToolbarModule, MatExpansionModule } from '@angular/material';
+import { CommonModule } from '@angular/common';
+import { ExperienceComponent } from './views/experience/experience.component';
+import { HomeComponent } from './views/home/home.component';
+import { StreamingComponent } from './views/streaming/streaming.component';
+import { AppRoutingModule } from './app.routing';
+import { ComingSoonComponent } from './views/home/coming-soon/coming-soon.component';
+import { BiographyComponent } from './views/biography/biography.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
+    CommonModule,
     BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+
+    MatButtonModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule,
+
+    RhombusShellModule.forRoot({
+      applicationInfo: {
+        name: 'Doug Williamson',
+        version: '1.0.0-beta.0',
+        logoUrl: '/assets/images/doug_williamson.jpg',
+        logoLetters: undefined,
+        twitterUrl: 'https://twitter.com/Rhombus_TV_',
+      },
+    }),
+
     AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    ExperienceComponent,
+    HomeComponent,
+    StreamingComponent,
+    ComingSoonComponent,
+    BiographyComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
