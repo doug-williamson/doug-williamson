f505d5c56ce4b7a76c0925f13ec3c25b848ae2eb
Doug Williamson <douglasmwilliamson@gmail.com>
2019-12-13 13:54:58 -0500

    Feature/doug williamson v1 (#1) (#2)
    * 1.0.0-beta.0 updates
    
    Updated primary views for Beta.0 release
    
    * 1.0.0-beta.0 updates

---
+++ src/app/app.routing.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ExperienceComponent } from './views/experience/experience.component';
+import { HomeComponent } from './views/home/home.component';
+import { StreamingComponent } from './views/streaming/streaming.component';
+import { BiographyComponent } from './views/biography/biography.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'biography',
+    component: BiographyComponent,
+  },
+  {
+    path: 'experience',
+    component: ExperienceComponent,
+  },
+  {
+    path: 'streaming',
+    component: StreamingComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
