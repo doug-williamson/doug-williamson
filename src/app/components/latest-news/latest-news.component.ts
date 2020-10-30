8a58be64eded4522cd07a67d850497368d6a0901
Doug Williamson <douglasmwilliamson@gmail.com>
2020-10-30 12:25:52 -0400

    Updated LatestNews (#42)
    Converted mat-list to table, added Firebase service

--- src/app/components/latest-news/latest-news.component.ts
+++ src/app/components/latest-news/latest-news.component.ts
@@ -1,26 +1,33 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { MediaObserver } from '@angular/flex-layout';
+import { ILatestNews, LatestNewsService } from './latest-news.service';
 
 @Component({
   selector: 'app-latest-news',
   templateUrl: './latest-news.component.html',
-  styleUrls: ['./latest-news.component.scss']
+  styleUrls: ['./latest-news.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class LatestNewsComponent implements OnInit {
 
-  items = [
-    {
-      name: "Doug's Blog is here!",
-      date: new Date('04/04/2020'),
-    },
-    {
-      name: "New domain: 'dougwilliamson.space'",
-      date: new Date('02/27/2020')
-    },
-  ];
+  displayColumns: string[] = ['timestamp', 'title'];
+  expandedLatestNews: ILatestNews | null;
 
-  constructor() { }
+  latestNews: ILatestNews[];
+
+  constructor(public media: MediaObserver, private latestNewsService: LatestNewsService) {}
 
   ngOnInit() {
+    this.latestNewsService.getLatestNews$().subscribe(res => {
+      this.latestNews = res;
+    });
   }
 
 }
